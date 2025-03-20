ff4379124a0d1a54a3305e2ba01546a386c251a6
Joshua Kirby <jojokirby88@gmail.com>
2025-03-20 20:28:18 +0000

    Configured SST.

--- sst.config.ts
+++ sst.config.ts
@@ -7,9 +7,25 @@ export default $config({
 			removal: input?.stage === "production" ? "retain" : "remove",
 			protect: ["production"].includes(input?.stage),
 			home: "aws",
+			providers: {
+				aws: {
+					region: "us-east-1",
+				},
+			},
 		}
 	},
 	async run() {
-		new sst.aws.Nextjs("MyWeb")
+		new sst.aws.Nextjs("NextJS", {
+			transform: {
+				server: {
+					timeout: "900 seconds",
+					runtime: "nodejs22.x",
+					architecture: "arm64",
+				},
+			},
+			dev: {
+				command: "pnpm exec next dev --turbo",
+			},
+		})
 	},
 })
