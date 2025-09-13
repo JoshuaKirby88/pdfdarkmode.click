a5ab9310d4e7e13eebadafa9d0d41108d9429303
Joshua Kirby <jojokirby88@gmail.com>
2025-09-13 20:26:18 +0900

    Downgraded next.

--- utils/env.ts
+++ utils/env.ts
@@ -2,16 +2,13 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-	server: {
-		CLOUDFLARE_API_TOKEN: z.string(),
-	},
+	server: {},
 	client: {
 		NEXT_PUBLIC_POSTHOG_KEY: z.string(),
 		NEXT_PUBLIC_POSTHOG_HOST: z.string(),
 		NEXT_PUBLIC_GITHUB_URL: z.string(),
 	},
 	runtimeEnv: {
-		CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
 		NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
 		NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 		NEXT_PUBLIC_GITHUB_URL: process.env.NEXT_PUBLIC_GITHUB_URL,
